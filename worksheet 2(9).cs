7adc4a43e0a31883b33bcfdb839e0a6eccfac37d
AlsanKhadka <alsankhadka18@gmail.com>
2025-02-16 09:36:07 +0545

    Add files via upload

---
+++ worksheet 2(9).cs
@@ -0,0 +1,24 @@
+﻿/* using multidimensional array to display this 
+ *   *
+ *   **
+ *   ***
+ *   ****
+ *   */
+public class Program
+{
+    public static void Main()
+    {
+        string[,] MulD = new string[4, 1];
+        string a = "*";
+        for(int i = 0; i < MulD.GetLength(0); i++)
+        {
+            for(int j = 0; j <= 0; j++)
+            {
+                MulD[i,j] = a ;
+                a = a + "*";
+                Console.Write($"{MulD[i,j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
