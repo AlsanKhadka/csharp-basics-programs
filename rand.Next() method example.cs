e672ebf5f7533c43e34c5e688c77a92c65e071d7
AlsanKhadka <alsankhadka18@gmail.com>
2025-02-18 08:35:06 +0545

    Add files via upload

---
+++ rand.Next() method example.cs
@@ -0,0 +1,26 @@
+﻿//Random class and Next method example
+public class Program
+{
+    public void Method1()
+    {
+        Console.WriteLine("The random number is ");
+        Random obj = new Random();
+        int A = obj.Next();
+        Console.WriteLine($"{A}");
+    }
+}
+public class Person
+{
+    public static void Main()
+    {
+       
+        Program obj1 = new Program();
+        obj1.Method1();
+        Console.WriteLine("The random numbers are ");
+        Random rand = new Random();
+        for(int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{rand.Next()}");
+        }
+    }
+}
