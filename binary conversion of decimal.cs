b5d6680696326e182a906fa019b2be805e35416e
AlsanKhadka <alsankhadka18@gmail.com>
2025-02-19 11:51:02 +0545

    Add files via upload

---
+++ binary conversion of decimal.cs
@@ -0,0 +1,23 @@
+﻿//print binary equvalent of a decimal number
+public class Program
+{
+    public static void Main()
+    {
+        int num = 20, a,i = 0;
+        int[] array1 = new int[10];
+        int temp = num;
+        while (num > 0)
+        {
+            a = num % 2;
+            array1[i] = a;
+            num = num / 2;
+            i++;
+        }
+        i--;
+        Console.Write($"The binary equivalent of {temp} is ");
+        while (i >= 0)
+        {
+            Console.Write($"{array1[i--]} ");
+        }
+    }
+}
