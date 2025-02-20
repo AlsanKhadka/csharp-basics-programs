81138d57adc24970f3ff4824f73d4c9217e5ad1e
AlsanKhadka <alsankhadka18@gmail.com>
2025-02-20 08:59:41 +0545

    Add files via upload

---
+++ size of the datatype.cs
@@ -0,0 +1,16 @@
+﻿//convert from double value to integer
+public class Program
+{
+    public static void Conversion(int a,int b=12)
+    {
+        double d = 1.223d;
+        a = (int)d;
+        float f = 1.22f;
+        b = f;
+        Console.WriteLine($"The conversion from double to int is {a} and the conversion from float to int is {b}");
+    }
+    public static void Main()
+    {
+        Program.Conversion(10);
+    }
+}
