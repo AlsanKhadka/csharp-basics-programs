2e3ecf1004069b9cbcd331e653661525dbdcbd28
AlsanKhadka <alsankhadka18@gmail.com>
2025-02-26 07:16:01 +0545

    Add files via upload

---
+++ c# program to display largest factor if the number is a prime number.cs
@@ -0,0 +1,26 @@
+﻿//c# program to display largest factor if the number is a prime number
+public class Program
+{
+    public static void Main()
+    {
+        int num,a=0;
+        Console.Write("Enter your number ");
+        num = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                a++;
+            }
+        }
+        if (a == 2)
+        {
+            Console.WriteLine($"The largest factor of prime number {num} is {num} ");
+        }
+        else
+        {
+            Console.WriteLine("The number is not a prime number ");
+        }
+
+    }
+}
