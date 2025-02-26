18640b3b20cdbcfdee5bc40bb745363aa1be805b
AlsanKhadka <alsankhadka18@gmail.com>
2025-02-26 18:23:54 +0545

    Add files via upload

---
+++ print even and odd numbers in a given range using nested for loop.cs
@@ -0,0 +1,37 @@
+﻿//print even and odd numbers in a given range using nested for loop
+public class Program
+{
+    public static void Main()
+    {
+        int terms, start,a=0,b=0;
+        Console.Write("Enter your starting number ");
+        start = int.Parse(Console.ReadLine());
+        Console.Write("Enter your terms ");
+        bool A = int.TryParse(Console.ReadLine(), out terms);
+        Console.WriteLine("The odd and even numbers are ");
+        for(int i = start; i <=terms; i++)
+        {
+            for (int j = i; j <= terms; j++)
+            {
+                if (i > start)
+                {
+                    break;
+                }
+                if (j % 2 != 0)
+                {
+
+                    Console.Write($"{j} ");
+                }
+            }
+                if (start == i)
+                {
+                    Console.WriteLine();
+                }
+                if (i % 2 == 0)
+                {
+                    
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
---
+++ print even number or odd number based on user input(like if user say Even then the even numbers should print.cs
@@ -0,0 +1,54 @@
+﻿/*print even number or odd number based on user input(like if user say Even then the even numbers should print
+ * if the user say odd then odd numbers should print) and take input like terms and starting number and also 
+ * consider the user need to restart the process then the use can do so.
+ */
+public class Program
+{
+    public static void Main()
+    {
+        string choice;
+        do
+        {
+            char name;
+            int start, term;
+            Console.Write("Enter what you want to do(even= E and odd= O ");
+            name = char.Parse(Console.ReadLine());
+            Console.Write("Enter your starting number ");
+            start = int.Parse(Console.ReadLine());
+            Console.Write("Enter your terms ");
+            bool A = int.TryParse(Console.ReadLine(), out term);
+            int a = 0;
+            int b = 0, c = 0;
+            for (int i = start; a < term; i++)
+            {
+            
+                if (name == 'E' && i % 2 == 0)
+                {
+                    if (b==0)
+                    {
+
+                        Console.Write("The even numbers are: ");
+                        b++;
+                    }
+                    Console.Write($"{i} ");
+                    a++;
+                }
+                if (name == 'O' && i % 2 != 0)
+                {
+                    if (c==0)
+                    {
+                        Console.Write("The odd numbers are ");
+                        c++;
+                    }
+                    Console.Write($"{i} ");
+                    a++;
+                }
+
+            }
+            Console.WriteLine();
+            Console.Write("You need to restart the process(Yes=Y,No=N) ");
+            choice = Console.ReadLine();
+        }
+        while (choice == "Y");
+    }
+}
