6cb96f52aec32bf42ceee63a8d2e052aac2e6ad7
AlsanKhadka <alsankhadka18@gmail.com>
2025-03-13 07:28:09 +0545

    Add files via upload

---
+++ print number in a right angle triangle format.cs
@@ -0,0 +1,36 @@
+﻿/* 1
+ * 23
+ * 456
+ * 78910
+ * 456
+ * 23
+ * 1*/
+public class Program
+{
+    public static void Main()
+    {
+        int a = 0 ;
+        for(int i = 1; i <= 4; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                a++;
+                Console.Write($"{a}");
+
+            }
+            Console.WriteLine();
+        }
+        a = a - 7; int b = 0;
+        for(int i = 3; i >= 1; i--)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                a++;
+                Console.Write($"{a}");
+            }
+            Console.WriteLine();
+            a = a - 5+ b;
+            b = 2;
+        }
+    }
+}
